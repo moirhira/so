7fc8cdea0aa22d4a3018b88805361589b6e44fa1
mohamed <irhirallah.mohammed@gmail.com>
2025-02-14 10:57:06 +0100

    load first spirete

--- src/main.c
+++ src/main.c
@@ -1,27 +1,41 @@
 #include "../libraries/minilibx-linux/mlx.h"
 #include "../includes/so_long.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-#define WIDTH   600
-#define HIGHT   400
+#define WIDTH   800
+#define HIGHT   600
 
+typedef struct s_game
+{
+    void *mlx_ptr;
+    void *win_ptr;
+    int sprite_w_h;
+    void *player_sprt;
+} t_game;
 
 int main() {
-    read_map();
-    // void *mlx_ptr;
-    // void *win_ptr;
+    if (!read_map())
+        printf("map problem!\n");
+    t_game *gameData = malloc(sizeof(t_game));
+    gameData->mlx_ptr = mlx_init();
+    if (!gameData->mlx_ptr)
+        return (1);
 
-
-    // mlx_ptr = mlx_init();
-    // if (!mlx_ptr)
-    //     return (1);
-
-    // win_ptr = mlx_new_window(mlx_ptr, WIDTH, HIGHT, "Game : moirhira");
-    // if (!win_ptr)
-    //     return (free(mlx_ptr),1);
-    // mlx_loop(mlx_ptr);
-    // mlx_destroy_window(mlx_ptr,win_ptr);
-    // mlx_destroy_display(mlx_ptr);
-    // free(mlx_ptr);
+    gameData->win_ptr = mlx_new_window(gameData->mlx_ptr, WIDTH, HIGHT, "Game : mohamed");
+    if (!gameData->win_ptr)
+        return (free(gameData->mlx_ptr),1);
+    gameData->sprite_w_h = 32;
+    gameData->player_sprt = mlx_xpm_file_to_image(gameData->mlx_ptr, "textures/player.xpm",&gameData->sprite_w_h,&gameData->sprite_w_h);
+    if (!gameData->player_sprt)
+    {
+        printf("eroo loading player!\n");
+        exit (1);
+    }
+    mlx_put_image_to_window(gameData->mlx_ptr, gameData->win_ptr, gameData->player_sprt,100 * gameData->sprite_w_h, 100 * gameData->sprite_w_h);
+    mlx_loop(gameData->mlx_ptr);
+    mlx_destroy_window(gameData->mlx_ptr,gameData->win_ptr);
+    mlx_destroy_display(gameData->mlx_ptr);
+    free(gameData->mlx_ptr);
     return 0;
 }
